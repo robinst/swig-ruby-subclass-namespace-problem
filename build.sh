b80e00e094780f3ae3282b17c3ae9715f3b063fc
Robin Stocker <robin@nibor.org>
2011-10-08 21:35:27 +0200

    Use variable for ruby binary

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 swig=swig2.0
 swigopts="-c++ -ruby -autorename"
+ruby="ruby"
 
 (cd base;
  $swig $swigopts -initname base -I. base.i;
- ruby extconf.rb && make;
+ $ruby extconf.rb && make;
 )
 (cd sub;
  $swig $swigopts -initname sub -I. -I../base sub.i;
  g++ -I../base -c -fPIC sub.cpp
- ruby extconf.rb --with-opt-include=../base && make;
+ $ruby extconf.rb --with-opt-include=../base && make;
 )
 cp base/base.so sub/sub.so .
